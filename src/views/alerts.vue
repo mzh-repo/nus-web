8166c91d25af4483f5fb822c8cbd77a07c35d34c
Qiu Jinhuai <qiujinhuai@mzhtechnologies.com>
2019-10-30 15:48:57 +0800

    update router

---
+++ src/views/alerts.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-container>
+
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
