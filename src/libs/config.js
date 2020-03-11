17f30686cd97ff4b683bf17c3b9767d6aeaf9e45
saxon <shenpy@mzhtechnologies.com>
2020-03-11 14:01:40 +0800

    add .prettierrc

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,7 +1,7 @@
 const url = {
-  // localUrl: 'http://192.168.2.111:8080', // mock-data
+  localUrl: 'http://137.132.174.66:8084', // mock-data
   // localUrl: 'http://192.168.2.100:8084',
-  localUrl: 'http://47.110.156.186:8084',
+  // localUrl: 'http://47.110.156.186:8084',
 };
 
 export default url;
